c105c7d3282b4e4874921eb8b8addda4d54ca2e3
Cristian Pessio <48696162+Venecio@users.noreply.github.com>
2020-12-04 19:58:05 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,372 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h> //Para las tildes
+#define MAX 100
+
+using namespace std;
+
+/* Consignas Ejercicio Nº
+1)Usuario asigna el tamaño del array.
+2)Llenar:
+        1)Usuario llena arreglo
+        2)llenar con numeros impares.
+        3)Llenar con numeros pares.
+        4)Numeros aleatorios.
+3)Buscar
+4)mostrar elemento
+5)ordenar > <
+6)insertar/modificar/eliminar.
+        1)1º
+        2)medio.
+        3)ultimo
+7)Mostrar todo.
+*/
+void Menu();
+void SubMenuLlenar();
+void SubMenuPMU();
+bool arrayVacio(int dimensionArray);
+void LlenarArray(int opcionDeLLenado, int dimensionArray);
+void MostrarTodosLosValoresDelVector(int dimensionArray);
+void MostrarElementoEnUnaPosicion(int dimensionArray);
+void BuscarUnValorDelArray(int dimensionArray);
+void OrdenarArray(int dimensionArray);
+int IME (int dimensionArray);
+void SubMenuAcciones();
+
+int arreglo[MAX]; //Arreglo Global
+
+int main()
+{
+    setlocale(LC_CTYPE, "Spanish"); //para las tildes.
+    char opcionSeguir;
+    int dimensionArray = 0, opcionMenu;
+
+    do
+    {
+        cout<<"\nINTRODUCIR LA DIMENSIÓN DEL ARRAY ANTES DE REALIZAR CUALQUIER OTRA OPERACIÓN"<<endl;
+        Menu();
+        cout << "Elija una opción del menu: ";
+        cin >>opcionMenu;
+        switch(opcionMenu){
+        case 1:
+            system("cls");
+            cout << "Asignando tamaño del array.." << endl;
+            if(arrayVacio(dimensionArray) == true)
+                system("cls");
+            {
+                cout<<"\nDigite tamaño del array :";
+                cin>>dimensionArray;
+            }
+            break;
+        case 2: // Insertar Valores en el array
+            system("cls");
+            cout << "Insertando valores del array.." << endl;
+            if(arrayVacio(dimensionArray) == false)
+                system("cls");
+            {
+                cout<<"\nElija una opción para llenar el arreglo: " << endl;
+                SubMenuLlenar();
+                cin>>opcionMenu;
+                LlenarArray(opcionMenu, dimensionArray);
+            }
+            break;
+
+
+        case 3:// buscar un valor dentro del array
+            system("cls");
+            if(arrayVacio(dimensionArray) == false){
+                BuscarUnValorDelArray(dimensionArray);
+            }
+            break;
+
+        case 4: // mostrar una posicion dentro del array
+            system("cls");
+            if(arrayVacio(dimensionArray) == false){
+                MostrarElementoEnUnaPosicion(dimensionArray);
+            }
+            break;
+
+        case 5: // Ordenar de mayor a menor
+            system("cls");
+            if(arrayVacio(dimensionArray) == false){
+                OrdenarArray(dimensionArray);
+            }
+        break;
+    case 6: //Insertar / modificar / eliminar
+            system("cls");
+            if(arrayVacio(dimensionArray) == false){
+                dimensionArray = IME(dimensionArray);
+
+            }
+        break;
+    case 7:
+        system("cls");
+        MostrarTodosLosValoresDelVector(dimensionArray);
+        break;
+    default:
+        cout << "La opción elegida no existe."<< endl;
+        break;
+    };
+
+    cout<<"\n\nDesea realizar otra operación? <s/n>: ";
+    fflush( stdin );
+    cin>>opcionSeguir;
+    fflush( stdin ); fflush( stdout );
+    system("cls");
+}while((opcionSeguir == 's') || (opcionSeguir == 'S') );
+
+system ("PAUSE");
+}
+
+
+// 2 - Funcion de llenado de array
+void LlenarArray(int opcionDeLLenado, int dimensionArray)
+{
+    int guardarValor;
+    switch(opcionDeLLenado){  //Opciones del subMenuLlenar
+    case 1:
+        for(int i= 0; i< dimensionArray; i++)
+        {
+            cout<<"\nIngrese el valor de la posición  " << i+1 << " : ";
+            cin>>guardarValor;
+            arreglo[i] = guardarValor;
+        }
+        break;
+    case 2:
+        system("cls");
+        cout<<"\nNúmeros impares aleatorios generados, volver a la opción 7 para ver los cambios realizados en el array.";
+        for(int i= 0; i< dimensionArray; i++)
+        {
+            guardarValor = rand();
+            if(guardarValor % 2 == 0)
+            {
+                while(guardarValor % 2 == 0)
+                {
+                    guardarValor = rand();
+                }
+            }
+            arreglo[i] = guardarValor;
+        }
+        break;
+    case 3:
+        cout<<"\nNúmeros pares aleatorios, volver a la opción 7 para ver los cambios realizados en el array.";
+        for(int i= 0; i< dimensionArray; i++)
+        {
+            guardarValor = rand();
+            if(guardarValor % 2 != 0)
+            {
+                while(guardarValor % 2 != 0)
+                {
+                    guardarValor = rand();
+                }
+            }
+            arreglo[i] = guardarValor;
+        }
+        break;
+    case 4:
+        cout<<"\nNúmeros aleatorios generados, volver a la opción 7 para ver los cambios realizados en el array.";
+        for(int i= 0; i< dimensionArray; i++)
+        {
+            arreglo[i] = 1+rand()%51;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+    //3 - Buscar un valor dentro del vector
+void BuscarUnValorDelArray(int dimensionArray){
+    int aux;
+    bool valorEncontrado = false;
+    cout<<"Ingrese el valor que desea buscar en el array: ";cin>>aux;
+
+    for (int i=0; i<dimensionArray; i++){
+        if(arreglo[i]==aux){
+            cout<<"El valor "<< aux <<" fue encontrado en la posición "<< i+1 << endl;
+            valorEncontrado = true;
+        }
+    }
+    if (valorEncontrado == false){
+        cout<< "El valor " << aux << " no fue encontrado en el array."<<endl;
+    }
+}
+
+
+    //4 - Muestra un elemento del vector segun la posicion (buscar)
+void MostrarElementoEnUnaPosicion(int dimensionArray){
+    int posicion;
+        cout<<"Ingrese la posición que desea ver del array: "; cin>>posicion;
+        fflush(stdin);
+
+        if(posicion < dimensionArray){
+            cout<<"El valor en la posición " << posicion << " es " << arreglo[posicion-1] <<endl;
+        } else {
+            cout << "La posición ingresada no existe. El arreglo solo tiene una dimensión de : " << dimensionArray << endl;
+        }
+}
+
+
+    // 5 - Ordenar array
+void OrdenarArray(int dimensionArray){
+    int aux;
+    cout<<"\nNúmeros ordenados, volver a la opción 7 para ver los cambios realizados en el array."<<endl;
+    for(int i=0; i<dimensionArray; i++){
+        for(int j=0; j<dimensionArray-1; j++){
+            if(arreglo[j]> arreglo[j+1]){
+                aux= arreglo[j];
+                arreglo[j] = arreglo[j+1];
+                arreglo[j+1] = aux;
+            }
+        }
+    }
+}
+
+    // 6 - Insertar / modificar / eliminar
+int IME (int dimensionArray){
+    int accion, posicion, valorAgregar;
+    int arregloAux[MAX];
+    SubMenuAcciones();
+    cout << "Ingrese la acción que desea realizar sobre el array: ";
+    cin >> accion; fflush( stdin );
+    if (accion > 0 && accion < 4 ){
+        cout << "La dimensión del array actual es de: " << dimensionArray << endl; fflush( stdin );
+    }
+
+    switch(accion){
+        case 1: //Insertar
+
+                cout << "La dimensión del nuevo array luego de la insersión sera de: " << dimensionArray+1 << endl;
+                cout << "Ingrese el valor que desea agregar: "; cin >> valorAgregar;
+
+                cout << "Ingrese la posición que desea afectar: "; cin >> posicion;
+
+            if(posicion > 0 && posicion <= dimensionArray+1 ){
+                //Haciendo copia de seguridad del viejo array
+                for (int i = 0; i < dimensionArray; i++){
+                    arregloAux[i] = arreglo[i];
+                }
+
+                //Para generar el nuevo array
+                for (int i = 0; i < dimensionArray+1; i++){
+                    if(i == posicion-1){
+                        arreglo[i] = valorAgregar;
+                    }
+                    if(i > posicion-1){
+                        arreglo[i] = arregloAux[i-1];
+                    }
+                }
+                dimensionArray = dimensionArray+1;
+            }else {
+                cout << "La posición a insertar esta fuera de rango." << endl;
+            }
+            break;
+        case 2: //Modificar
+
+                cout << "Ingrese el valor que desea agregar: "; cin >> valorAgregar;
+
+                cout << "Ingrese la posicion que desea afectar: "; cin >> posicion;
+
+            if(posicion > 0 && posicion <= dimensionArray ){
+                //Para generar el nuevo array
+                for (int i = 0; i < dimensionArray; i++){
+                    if(i == posicion-1){
+                        arreglo[i] = valorAgregar;
+                    }
+                }
+            }else {
+                cout << "La posición a modificar está fuera de rango." << endl;
+            }
+            break;
+
+        case 3: // Eliminar
+                cout << "La dimensión del nuevo array sera de " << dimensionArray-1<<endl ; fflush(stdin);
+                cout << "Ingrese la posición que desea eliminar el valor: " ; cin >> posicion; fflush(stdin);
+
+            if(posicion > 0 && posicion <= dimensionArray ){
+                //Haciendo copia de seguridad del viejo array
+                for (int i = 0; i < dimensionArray; i++){
+                    arregloAux[i] = arreglo[i];
+                }
+                //Para generar el nuevo array
+                for (int i = 0; i < dimensionArray-1; i++){
+                    if(dimensionArray != posicion){
+                        if(i >= posicion-1){
+                            arreglo[i] = arregloAux[i+1];
+                        }
+                    }
+                }
+
+                dimensionArray = dimensionArray-1;
+            }else {
+                cout << "La posición a eliminar esta fuera de rango." << endl;
+            }
+
+            break;
+        default:
+            cout << "La acción ingresada no es valida." << endl;
+            break;
+    }
+        return dimensionArray;
+}
+
+
+
+    //7 - Muestra  todos los elementos del vector
+void MostrarTodosLosValoresDelVector(int dimensionArray)
+{
+    cout<< "Imprimiendo todos los valores del array"<<endl;
+    for(int i = 0; i < dimensionArray; i++){
+        printf("\n\t -> Posicion [%d]: %d", i, arreglo[i]);
+    }
+}
+
+
+    // Funcion validacionDeDimensionDeArray
+bool arrayVacio(int dimensionArray)
+{
+    if(dimensionArray <= 0)
+    {
+        cout<<"\nEl arreglo esta vacio."<<endl;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// Funciones de menus
+void Menu()
+{
+    cout<<"1)Asignar el tamaño del array:"<<endl;
+    cout<<"2)Llenar el array:"<<endl;
+    cout<<"3)Buscar un valor dentro del array."<<endl;
+    cout<<"4)Mostrar el elemento."<<endl;
+    cout<<"5)Ordenar el array de > <"<<endl;
+    cout<<"6)Insertar / Modificar / Eliminar "<<endl;
+    cout<<"7)Mostrar todos los valores del array.\n"<<endl;
+}
+void SubMenuLlenar()
+{
+    cout<<"\t1)LLenar array de forma manual"<<endl;
+    cout<<"\t2)Llenar array con números impares."<<endl;
+    cout<<"\t3)Llenar array con números pares."<<endl;
+    cout<<"\t4)Llenar array con números aleatorios.\n"<<endl;
+}
+
+void SubMenuPMU()
+{
+    cout<< "Elija una opción que desee aplicarle la accion: " << endl;
+    cout<<"\t1) Primero"<<endl<<"\t2) Medio."<<endl<<"\t3) Ultimo\n"<<endl;
+}
+
+void SubMenuAcciones(){
+    cout << "\nElija una de las siguientes opciones: "<< endl;
+    cout << "\t1) Ingresar nuevo elemento en el array"<< endl;
+    cout << "\t2) Modificar elemento existente del array" << endl;
+    cout << "\t3) Eliminar un elemento del array"<< endl;
+}
